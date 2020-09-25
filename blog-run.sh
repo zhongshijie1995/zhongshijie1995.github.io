5064c884d2e4b2e045e041c9242d9c6d8158e289
zhongshijie <zhongshijie1995@outlook.com>
2020-09-25 18:56:28 +0800

    Auto+1

--- blog-run.sh
+++
@@ -1,4 +0,0 @@
-cd Desktop/01-Project/zhongshijie || sh auto-run.sh
-cd Desktop/01-Project/mirrors-pic || sh auto-run.sh
-source ~/.bash_profile
-cd Desktop/01-Project/zhongshijie || python auto-deploy.py
