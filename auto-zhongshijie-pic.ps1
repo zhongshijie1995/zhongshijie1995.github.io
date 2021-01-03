07b2847f8f3b22aec1638c59af1134008d3b7a95
zhongshijie <zhongshijie1995@outlook.com>
2021-01-03 15:56:04 +0800

    Auto+1

--- auto-zhongshijie-pic.ps1
+++ auto-zhongshijie-pic.ps1
@@ -1,3 +1,8 @@
-../mirrors-pic/auto-commit.ps1
+echo " ----------- 提交 mirrors-pic ----------- "
+cd ../mirrors-pic
+./auto-commit.ps1
+cd ../zhongshijie
+echo " ----------- 提交 zhongshijie ----------- "
+python replace-pic.py
 ./auto-commit.ps1
 python auto-deploy.py
---
+++ replace-pic.py
@@ -0,0 +1,26 @@
+import os
+ 
+file_path = []
+fileDir = "source/_posts"
+
+def func():
+    for root, dirs, files in os.walk(fileDir):  
+        for fileitem in files:
+            file_path.append('{}/{}'.format(root,fileitem))
+
+
+def alter(file,old_str,new_str):
+    file_data = ""
+    with open(file, "r", encoding='utf-8') as f:
+        for line in f:
+            if old_str in line:
+                line = line.replace(old_str,new_str)
+            file_data += line
+    with open(file,"w", encoding='utf-8') as f:
+        f.write(file_data)
+
+
+if __name__ == "__main__":
+    func()
+    for item in file_path:
+        alter(item,"D:/01-Project/Blog/mirrors-pic/img/","https://zhongshijie.gitee.io/mirrors-pic/img/")
