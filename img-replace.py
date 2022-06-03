893aab8f14b88ee7f91717b4384f7b8607da0874
钟世杰 <zhongshijie1995@outlook.com>
2022-06-03 18:34:57 +0800

    AutoCommit

--- img-replace.py
+++ img-replace.py
@@ -1,27 +1,28 @@
 import os
- 
+
 file_path = []
 fileDir = "source/_posts"
 
+
 def func():
-    for root, dirs, files in os.walk(fileDir):  
-        for fileitem in files:
-            file_path.append('{}/{}'.format(root,fileitem))
+    for root, dirs, files in os.walk(fileDir):
+        for file_item in files:
+            file_path.append('{}/{}'.format(root, file_item))
 
 
-def alter(file,old_str,new_str):
+def alter(file, old_str, new_str):
     file_data = ""
     with open(file, "r", encoding='utf-8') as f:
         for line in f:
             if old_str in line:
-                line = line.replace(old_str,new_str)
+                line = line.replace(old_str, new_str)
             file_data += line
-    with open(file,"w", encoding='utf-8') as f:
+    with open(file, "w", encoding='utf-8') as f:
         f.write(file_data)
 
 
 if __name__ == "__main__":
     func()
     for item in file_path:
-        alter(item,"D:/01-Project/Blog/mirrors-pic/img/","https://zhongshijie1995.github.io/zhongshijie-pic/img/")
-        alter(item,"/Users/zhongshijie/Desktop/01-Project/zhongshijie-pic/img/", "https://zhongshijie1995.github.io/zhongshijie-pic/img/")
+        alter(item, "D:/01-Project/Blog/mirrors-pic/img/", "https://zhongshijie1995.github.io/zhongshijie-pic/img/")
+        alter(item, "](img/", "https://zhongshijie1995.github.io/zhongshijie-pic/img/")
