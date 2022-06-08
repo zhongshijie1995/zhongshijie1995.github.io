380bca2b75dddd1dc7e22c04399eff258a1d3864
钟世杰 <zhongshijie1995@outlook.com>
2022-06-09 00:00:46 +0800

    AutoCommit

--- deploy-MacOS-IP.sh
+++ deploy-MacOS-IP.sh
@@ -1,7 +1,8 @@
 target_md="source/about/index.md"
-NowIP="http://"`curl https://ifconfig.me`":"
+NowIP=`curl https://ifconfig.me`
+NowIP=`echo $NowIP | grep -E "((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}"`
 echo '获取到当前外网IP：'$NowIP
-YetIP=`grep -m 1 -Eo "http://*.*.*.*:" $target_md`
+YetIP=`grep -m 1 -Eo "((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}" $target_md`
 echo '目前在显示外网IP：'$YetIP
 if [ "$NowIP" != "$YetIP" ];then
   sed -i "" 's#'"$YetIP"'#'"$NowIP"'#' $target_md
