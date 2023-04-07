19336722f08677b2da7cbfc079d1a031b8676ba5
钟世杰 <zhongshijie1995@outlook.com>
2023-04-07 23:56:50 +0800

    AutoCommit

--- deploy-MacOS-All.sh
+++ deploy-MacOS-All.sh
@@ -5,6 +5,6 @@ sh ./deploy-MacOS-IP.sh
 # 提交图片仓库代码
 cd ../zhongshijie-pic && git pull && git add .&& git commit -m 'AutoCommit' && git push
 # 提交博客仓库代码
-cd ../zhongshijie1995.github.io && git pull && git add .&& git commit -m 'AutoCommit' && git push
+cd ../zhongshijie1995.github.io && git pull && git add . && git commit -m 'AutoCommit' && git push
 # 部署博客
 npx hexo clean && npx hexo g && npx hexo deploy && npx hexo clean
